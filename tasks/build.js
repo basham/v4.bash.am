64d88edea5d1138b6ad94cc8dd1fdd4920eef3cb
Chris Basham <chris@bash.am>
2015-03-19 01:07:22 -0400

    Center align article headings

--- tasks/build.js
+++ tasks/build.js
@@ -27,6 +27,17 @@ Handlebars.registerHelper('formatDate', function(date) {
   return moment(date).format('MMMM D, YYYY');
 });
 
+Handlebars.registerHelper('ifIsInCollection', function(name, options) {
+  var collections = options.data.root.collection;
+  var contains = collections.indexOf(name) >= 0;
+  if(contains) {
+    return options.fn(this);
+  }
+  else {
+    return options.inverse(this);
+  }
+});
+
 Handlebars.registerHelper('log', function(content) {
   console.log(content);
 });
