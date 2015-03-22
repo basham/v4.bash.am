3da642b61f690bb7f6316b25b55929ad15dffb5c
Chris Basham <chris@bash.am>
2015-03-21 23:15:12 -0400

    Refactor handlebars and marked build code

--- tasks/build.js
+++ tasks/build.js
@@ -18,119 +18,10 @@ var less = require('metalsmith-less');
 var autoprefixer = require('metalsmith-autoprefixer');
 var inlineStyles = require('./plugins/inline-styles');
 
-//
-// Handlebars
-//
-var Handlebars = require('handlebars');
-var moment = require('moment');
+// Configs
+require('./config/handlebars');
+var renderer = require('./config/marked');
 
-Handlebars.registerHelper('formatDate', function(date) {
-  return moment(date).format('MMMM D, YYYY');
-});
-
-Handlebars.registerHelper('ifIsInCollection', function(name, options) {
-  var collections = options.data.root.collection;
-  var contains = collections.indexOf(name) >= 0;
-  if(contains) {
-    return options.fn(this);
-  }
-  else {
-    return options.inverse(this);
-  }
-});
-
-Handlebars.registerHelper('log', function(content) {
-  console.log(content);
-});
-
-//
-// Markdown renderer
-//
-var marked = require('marked');
-var renderer = new marked.Renderer();
-var classNames = require('classnames');
-var highlight = require('highlight.js');
-highlight.configure({
-  classPrefix: 'Code-part Code-part--'
-});
-
-renderer.code = function(code, lang, escaped) {
-  if (this.options.highlight) {
-    var out = this.options.highlight(code, lang);
-    if (out != null && out !== code) {
-      escaped = true;
-      code = out;
-    }
-  }
-
-  var cn = {
-    'Code': true
-  };
-  if(!!lang) {
-    var name = 'Code--' + escape(lang, true);
-    cn[name] = true;
-  }
-  var cn = classNames(cn);
-
-  return '<pre class="Article-codeFigure"><code class="' + cn + '">'
-    + (escaped ? code : escape(code, true))
-    + '\n</code></pre>\n';
-};
-
-renderer.codespan = function(text) {
-  return '<code class="Code Code--inline">' + text + '</code>';
-};
-
-renderer.heading = function(text, level, raw) {
-  var type = 'h' + level;
-  var id = this.options.headerPrefix + raw.toLowerCase().replace(/[^\w]+/g, '-');
-  var idAttr = level === 2 ? ' id="' + id + '"' : '';
-  var cn = 'Article-' + type;
-  return '<' + type + ' class="' + cn + '"' + idAttr + '>'
-    + text
-    + '</' + type + '>\n';
-};
-
-renderer.list = function(body, ordered) {
-  var type = ordered ? 'ol' : 'ul';
-  return '<' + type + ' class="Article-list">\n' + body + '</' + type + '>\n';
-};
-
-renderer.listitem = function(text) {
-  return '<li class="Article-listItem">' + text + '</li>\n';
-};
-
-renderer.paragraph = function(text) {
-  return '<p class="Article-paragraph">' + text + '</p>\n';
-};
-
-renderer.table = function(header, body) {
-  return '<div class="Article-tableFigure">\n'
-  + '<table class="Article-table">\n'
-  + '<thead>\n'
-  + header
-  + '</thead>\n'
-  + '<tbody>\n'
-  + body
-  + '</tbody>\n'
-  + '</table>\n'
-  + '</div>\n';
-};
-
-renderer.tablecell = function(content, flags) {
-  var type = flags.header ? 'th' : 'td';
-  var cn = classNames({
-    'Article-tableCell': true,
-    'Article-tableCell--header': flags.header,
-    'Article-tableCell--center': flags.align === 'center',
-    'Article-tableCell--right': flags.align === 'right'
-  });
-  return '<' + type + ' class="' + cn + '">' + content + '</' + type + '>\n';
-};
-
-//
-// Task
-//
 gulp.task('build', function(callback) {
   Metalsmith('./')
     // CSS
@@ -171,9 +62,6 @@ gulp.task('build', function(callback) {
       }
     }))
     .use(markdown({
-      highlight: function(code) {
-        return highlight.highlightAuto(code).value;
-      },
       renderer: renderer,
       smartypants: true
     }))
---
+++ tasks/config/marked.js
@@ -0,0 +1,87 @@
+var marked = require('marked');
+var highlight = require('highlight.js');
+var classNames = require('classnames');
+
+highlight.configure({
+  classPrefix: 'Code-part Code-part--'
+});
+
+var renderer = new marked.Renderer();
+
+renderer.code = function(code, lang, escaped) {
+  if(highlight) {
+    var out = highlight.highlightAuto(code).value;
+    if(out != null && out !== code) {
+      escaped = true;
+      code = out;
+    }
+  }
+
+  var cn = {
+    'Code': true
+  };
+
+  if(!!lang) {
+    var name = 'Code--' + escape(lang, true);
+    cn[name] = true;
+  }
+
+  var cn = classNames(cn);
+
+  return '<pre class="Article-codeFigure"><code class="' + cn + '">'
+    + (escaped ? code : escape(code, true))
+    + '</code></pre>';
+};
+
+renderer.codespan = function(text) {
+  return '<code class="Code Code--inline">' + text + '</code>';
+};
+
+renderer.heading = function(text, level, raw) {
+  var type = 'h' + level;
+  var id = this.options.headerPrefix + raw.toLowerCase().replace(/[^\w]+/g, '-');
+  var idAttr = level === 2 ? ' id="' + id + '"' : '';
+  var cn = 'Article-' + type;
+  return '<' + type + ' class="' + cn + '"' + idAttr + '>'
+    + text
+    + '</' + type + '>';
+};
+
+renderer.list = function(body, ordered) {
+  var type = ordered ? 'ol' : 'ul';
+  return '<' + type + ' class="Article-list">' + body + '</' + type + '>';
+};
+
+renderer.listitem = function(text) {
+  return '<li class="Article-listItem">' + text + '</li>';
+};
+
+renderer.paragraph = function(text) {
+  return '<p class="Article-paragraph">' + text + '</p>';
+};
+
+renderer.table = function(header, body) {
+  return '<div class="Article-tableFigure">'
+  + '<table class="Article-table">'
+  + '<thead>'
+  + header
+  + '</thead>'
+  + '<tbody>'
+  + body
+  + '</tbody>'
+  + '</table>'
+  + '</div>';
+};
+
+renderer.tablecell = function(content, flags) {
+  var type = flags.header ? 'th' : 'td';
+  var cn = classNames({
+    'Article-tableCell': true,
+    'Article-tableCell--header': flags.header,
+    'Article-tableCell--center': flags.align === 'center',
+    'Article-tableCell--right': flags.align === 'right'
+  });
+  return '<' + type + ' class="' + cn + '">' + content + '</' + type + '>';
+};
+
+module.exports = renderer;
