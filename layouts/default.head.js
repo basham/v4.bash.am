e70132ca78a16e57bad4e5dafbe0d56ad91b64a5
Chris Basham <chris@bash.am>
2019-01-28 21:09:18 -0500

    Update url to https

--- layouts/default.head.js
+++ layouts/default.head.js
@@ -1,1 +1,1 @@
-loadCSS('http://fonts.googleapis.com/css?family=Merriweather:400italic,400,700|Inconsolata:400,700');
+loadCSS('https://fonts.googleapis.com/css?family=Merriweather:400italic,400,700|Inconsolata:400,700');
